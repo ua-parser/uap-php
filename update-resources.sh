07f8ea5eee35f45c0d587ccc7d526e459dfdee8b
Lars Strojny <lars.strojny@internations.org>
2019-01-28 11:41:01 +0100

    Auto-tag new version on update

--- update-resources.sh
+++ update-resources.sh
@@ -4,6 +4,8 @@ git submodule --quiet foreach git pull --quiet
 bin/uaparser ua-parser:update
 if phpunit --stop-on-failure &> /dev/null; then
     git commit --quiet -a -m "Scheduled resource update" && git push --quiet origin master
+    new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+    git tag $new_version && git push origin $new_version
 else
     echo "Could not update resources"
 fi
