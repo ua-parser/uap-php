c7c959ab0d60750bb1a114f8083921d8eac936f8
Lars Strojny <lars.strojny@internations.org>
2019-01-29 13:26:26 +0100

    Fix script

--- update-resources.sh
+++ update-resources.sh
@@ -3,7 +3,7 @@ git pull --quiet
 git submodule --quiet foreach git pull --quiet
 bin/uaparser ua-parser:update
 output=`phpunit --stop-on-failure &> /dev/stdout`
-if [ $? -gt 0 ] ; then
+if [ $? -eq 0 ] ; then
     git commit --quiet -a -m "Scheduled resource update" && git push --quiet origin master
     new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
     git tag $new_version && git push origin $new_version
