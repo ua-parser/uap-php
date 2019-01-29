851915a08d45055feaad1b6ff3074143df07a11e
Lars Strojny <lars.strojny@internations.org>
2019-01-29 13:24:04 +0100

    Scheduled resource update

--- update-resources.sh
+++ update-resources.sh
@@ -2,10 +2,13 @@
 git pull --quiet
 git submodule --quiet foreach git pull --quiet
 bin/uaparser ua-parser:update
-if phpunit --stop-on-failure &> /dev/null; then
+output=`phpunit --stop-on-failure &> /dev/stdout`
+if [ $? -gt 0 ] ; then
     git commit --quiet -a -m "Scheduled resource update" && git push --quiet origin master
     new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
     git tag $new_version && git push origin $new_version
 else
     echo "Could not update resources"
+    echo
+    echo "$output"
 fi
