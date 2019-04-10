335c525b682a273e75305bae19ee3361dda55f8a
Edward HeXuefei <xuphey@gmail.com>
2019-04-10 14:19:08 +0800

    autoload Reader classes from a custom folder

---
+++ src/Util/Logfile/Custom/NginxCustomLogFormatReader.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * ua-parser
+ *
+ * Copyright (c) 2011-2012 Dave Olsen, http://dmolsen.com
+ *
+ * Released under the MIT license
+ */
+namespace UAParser\Util\Logfile;
+
+class NginxCustomLogFormatReader extends AbstractReader
+{
+    protected function getRegex()
+    {
+        return '@^
+            \[(?:[^:]+):(?:\d+:\d+:\d+) \s+ (?:[^\]]+)\]            # Date/time
+            \s+
+            (?:\S+)
+            \s+
+            (?:\S+)                                                 # IP
+            \s+
+            (?:\S+)
+            \s+
+            (?:\S+)
+            \s+
+            (?:\S+)
+            \s+
+            \"(?:\S+)\s(?:.*?)                                      # Verb
+            \s+
+            (?:\S+)\"                                               # Path
+            \s+
+            (?:\S+)                                                 # Response
+            \s+
+            (?:\S+)                                                 # Length
+            \s+
+            (?:\".*?\")                                             # Referrer
+            \s+
+            \"(?P<userAgentString>.*?)\"                            # User Agent
+            \s+
+            \"(?:\S+)\"
+        $@x';
+    }
+}
