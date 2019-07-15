2b7130ce5030a880e9d6c4d8463fd181dfd94711
Lars Strojny <lars.strojny@internations.org>
2019-07-15 16:19:26 +0200

    More robustness

--- update-resources.sh
+++ update-resources.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
-set -e
-git pull --quiet > /dev/null
-git submodule --quiet foreach git pull --quiet > /dev/null
-bin/uaparser ua-parser:update
-output=`vendor/bin/phpunit --stop-on-failure &> /dev/stdout`
-if [ $? -eq 0 ] ; then
-    git commit --quiet -a -m "Scheduled resource update" && \
-        git push --quiet origin master && \
-        new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'` && \
-        git tag $new_version && git push origin $new_version
-else
-    echo "Could not update resources"
-    echo
-    echo "$output"
-fi
+
+function try_catch() {
+    output=`$@ &>/dev/stdout`
+    if [ $? -gt 0 ]; then
+        echo -e "Command $@ failed\n\n$output"
+        exit 1
+    fi
+}
+
+try_catch git pull
+try_catch git submodule foreach git pull
+try_catch bin/uaparser ua-parser:update
+try_catch vendor/bin/phpunit --stop-on-failure
+git commit --quiet -a -m "Scheduled resource update"
+git push --quiet origin master && \
+new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'` && \
+git tag $new_version && git push origin $new_version
