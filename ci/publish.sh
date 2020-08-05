e15043f838a7584b2f614caae8c7ac5f17bc7e11
Lars Strojny <lars.strojny@internations.org>
2020-08-05 19:39:51 +0200

    Prefix for non-master branches

--- ci/publish.sh
+++ ci/publish.sh
@@ -7,17 +7,24 @@ if [ "$UPDATE_RESOURCES" != "true" ]; then
     exit 0
 fi
 
+test_prefix=""
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    test_prefix=test-
+fi
+
 if git diff-index --quiet HEAD; then
     git config --global user.email travis@travis-ci.org
     git config --global user.name "Travis CI"
 
-    git remote add upstream https://${GITHUB_TOKEN}@github.com/your_username/your_repo.git git
+    git remote add upstream https://${GITHUB_TOKEN}@github.com/ua-parser/uap-php.git
 
     git commit -a -m "Automatic resource update"
     git push upstream ${TRAVIS_BRANCH}
-    new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-    git tag test-$new_version
-    git push upstream test-$new_version
+
+    new_version=$test_prefix`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+
+    git tag $new_version
+    git push upstream $new_version
 
     echo "$new_version published"
 else
