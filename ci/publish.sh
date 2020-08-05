58f8294bf4d2d6cf10b3a4666aba38c910ad193c
Lars Strojny <lars.strojny@internations.org>
2020-08-05 19:20:38 +0200

    Next attempt

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
 set -x
+set -e
 
 if [ "$UPDATE_RESOURCES" != "true" ]; then
     echo "Skip updating resources. UPDATE_RESOURCES not set to true"
     exit 0
 fi
 
-
 git config --global user.email travis@travis-ci.org
 git config --global user.name "Travis CI"
 git status
 env
+new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+
+echo $new_version
+
 exit
 
-
-git commit -a -m "Scheduled resource update"
-if [ $? -gt 0 ]; then
-    rev=$(git rev-parse --short HEAD)
+if git diff-index --quiet HEAD; then
+    git commit -a -m "Automatic resource update"
     try_catch git push origin master
     new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
     try_catch git tag $new_version
--- ci/update.sh
+++ ci/update.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 set -x
+set -e
 
 if [ "$UPDATE_RESOURCES" != "true" ]; then
     echo "Skip updating resources. UPDATE_RESOURCES not set to true"
     exit 0
 fi
 
-git submodule foreach git pull origin master
+git submodule foreach git pull origin master --ff-only
 bin/uaparser ua-parser:convert uap-core/regexes.yaml
