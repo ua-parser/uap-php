4f9483855d46bc473e16af2cbc29dea1d624562b
Lars Strojny <lars.strojny@internations.org>
2020-08-05 18:42:50 +0200

    Specify php when running the scripts

--- update-resources.sh
+++ update-resources.sh
@@ -10,8 +10,8 @@ function try_catch() {
 
 try_catch git pull
 try_catch git submodule foreach git pull origin master
-try_catch bin/uaparser ua-parser:convert uap-core/regexes.yaml
-try_catch vendor/bin/phpunit --stop-on-failure
+try_catch php bin/uaparser ua-parser:convert uap-core/regexes.yaml
+try_catch php vendor/bin/phpunit --stop-on-failure
 try_catch git commit -a -m "Scheduled resource update"
 try_catch git push origin master
 new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
