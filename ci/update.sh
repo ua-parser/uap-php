37c1697ececae980c5fc52a981c3990023c2bead
Lars Strojny <lars.strojny@internations.org>
2020-08-05 21:47:30 +0200

    another try

--- ci/update.sh
+++ ci/update.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#! /bin/bash
 set -x
 set -e
 
@@ -7,5 +7,29 @@ if [ "$UPDATE_RESOURCES" != "true" ]; then
     exit 0
 fi
 
+test_prefix=""
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    test_prefix=test-
+fi
+
 git submodule foreach git pull origin master --ff-only
 bin/uaparser ua-parser:convert uap-core/regexes.yaml
+
+if git diff-index --quiet HEAD; then
+    git config --global user.email travis@travis-ci.org
+    git config --global user.name "Travis CI"
+
+    git remote add upstream https://${GITHUB_TOKEN}@github.com/ua-parser/uap-php.git
+
+    git commit -a -m "Automatic resource update"
+    git push upstream ${TRAVIS_BRANCH}
+
+    new_version=$test_prefix`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+
+    git tag $new_version
+    git push upstream $new_version
+
+    echo "$new_version published"
+else
+    echo "No resource update necessary"
+fi
