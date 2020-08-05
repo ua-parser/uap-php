48a6fca62a244b03a3b7a8a523745f189de68acf
Lars Strojny <lars.strojny@internations.org>
2020-08-05 19:26:26 +0200

    more work

--- ci/publish.sh
+++ ci/publish.sh
@@ -7,22 +7,15 @@ if [ "$UPDATE_RESOURCES" != "true" ]; then
     exit 0
 fi
 
-git config --global user.email travis@travis-ci.org
-git config --global user.name "Travis CI"
-git status
-env
-new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-
-echo $new_version
-
-exit
-
 if git diff-index --quiet HEAD; then
+    git config --global user.email travis@travis-ci.org
+    git config --global user.name "Travis CI"
     git commit -a -m "Automatic resource update"
-    try_catch git push origin master
+    git push origin master
     new_version=`git tag | sort --version-sort | tail -n 1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
-    try_catch git tag $new_version
+    git tag $new_version
     git push origin $new_version
+
     echo "$new_version published"
 else
     echo "No resource update necessary"
